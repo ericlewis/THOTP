e2ed2bd6f549aa2bd1328565ca1cc3f07fa786b8
Eric Lewis <ericlewis@users.noreply.github.com>
2019-09-19 04:24:32 -0400

    fix

--- Sources/THOTP/Extensions/PasswordProtocol+URL.swift
+++ Sources/THOTP/Extensions/PasswordProtocol+URL.swift
@@ -4,18 +4,20 @@ import URL_QueryItem
 public extension PasswordProtocol {
     var absoluteURL: URL {
         var components = URLComponents()
+        
         components.scheme = "otpauth"
         components.host = generator.generatorAlgorithm.stringValue
+        components.path = "/"
         
         var queryItems: [URLQueryItem] = [
             URLQueryItem(key: Key.secret, value: "")
         ]
         
         if let issuer = self.issuer {
-            components.host = "\(issuer):\(name)"
+            components.path += "\(issuer):\(name)"
             queryItems.append(URLQueryItem(key: Key.issuer, value: issuer))
         } else {
-            components.host = name
+            components.path += name
         }
         
         if let image = self.image {
